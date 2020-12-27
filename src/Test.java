6079f699c3a1845c13f07b4ec657ad7768eefcc0
1800300209 <1521563125@qq.com>
2020-12-27 15:09:26 +0800

    读取Excel文件并对输入的用户名密码进行判断

--- src/Test.java
+++ src/Test.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Test {
@@ -10,5 +11,19 @@ public class Test {
         System.out.println("请输入密码：");
         String password = sc.next();
         System.out.println("您输入的密码为：" + password);
+
+        File f = new File("D:\\Shop\\src\\massage.xlsx");
+        ReadExcel read = new ReadExcel();
+        User user[] = read.readExcel(f);
+        int flag = 0;
+        for (int i = 0; i < user.length; i++) {
+            if (username.equals(user[i].getUsername()) && password.equals(user[i].getPassword())) {
+                flag++;
+            }
+        }
+        if (flag > 0) {
+            System.out.println("登陆成功！");
+        } else
+            System.out.println("登陆失败！");
     }
 }
